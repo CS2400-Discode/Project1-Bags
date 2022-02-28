db1ee38fb2f4d6ac35a4c508422d2802b3383551
Owen Lovett <olovett214@gmail.com>
2022-02-28 11:47:04 -0800

    added comments

--- src/Bags/BagDriver.java
+++ src/Bags/BagDriver.java
@@ -10,7 +10,8 @@ public class BagDriver {
         System.out.println("Which bag type? Enter 1 for ResizableArrayBag or 2 for LinkedBag:");
 
         bagType = k.nextInt();
-
+        
+        //ResizableArrayBag
         if (bagType == 1){
 
             int entries; 
@@ -19,7 +20,8 @@ public class BagDriver {
 
             System.out.println("How many entries would you like to enter for Bag 1?");
             entries = k.nextInt();
-
+            
+            //add objects to bag 1
             for (int i = 0; i < entries; i++){
                 System.out.println("Please enter an entry for Bag 1:");
                 bag1.add(k.next());
@@ -28,21 +30,26 @@ public class BagDriver {
             System.out.println("How many entries would you like to enter for Bag 2?");
             entries = k.nextInt();
 
+            //add objects to bag 2
             for (int i = 0; i < entries; i++){
                 System.out.println("Please enter an entry for Bag 2:");
                 bag2.add(k.next());
             }
 
+            //store values from methods in new bags
             BagInterface everything = bag1.union(bag2);
             BagInterface commonItems = bag1.intersection(bag2);
             BagInterface leftOver1 = bag1.difference(bag2);
             BagInterface leftOver2 = bag2.difference(bag1);
 
+            //print out objects in the bags
             System.out.println("Union of bag1 and bag2: " + Arrays.toString(everything.toArray()));
             System.out.println("Intersection of bag1 and bag2: " + Arrays.toString(commonItems.toArray()));
             System.out.println("Difference of bag1 and bag2: " + Arrays.toString(leftOver1.toArray()));
             System.out.println("Difference of bag2 and bag1: " + Arrays.toString(leftOver2.toArray()));
         }
+        
+        //LinkedBag
         else if (bagType == 2){
             
             int entries2; 
@@ -52,6 +59,7 @@ public class BagDriver {
             System.out.println("How many entries would you like to enter for Bag 1?");
             entries2 = k.nextInt();
 
+            //add objects to bag 1
             for (int i = 0; i < entries2; i++){
                 System.out.println("Please enter an entry for Bag 1:");
                 bag1b.add(k.next());
@@ -60,21 +68,26 @@ public class BagDriver {
             System.out.println("How many entries would you like to enter for Bag 2?");
             entries2 = k.nextInt();
 
+            //add objects to bag 2
             for (int i = 0; i < entries2; i++){
                 System.out.println("Please enter an entry for Bag 2:");
                 bag2b.add(k.next());
             }
 
+            //store values from methods in new bags
             BagInterface everythingB = bag1b.union(bag2b);
             BagInterface commonItemsB = bag1b.intersection(bag2b);
             BagInterface leftOver1B = bag1b.difference(bag2b);
             BagInterface leftOver2B = bag2b.difference(bag1b);
 
+            //print out objects in the bags
             System.out.println("Union of bag1 and bag2: " + Arrays.toString(everythingB.toArray()));
             System.out.println("Intersection of bag1 and bag2: " + Arrays.toString(commonItemsB.toArray()));
             System.out.println("Difference of bag1 and bag2: " + Arrays.toString(leftOver1B.toArray()));
             System.out.println("Difference of bag2 and bag1: " + Arrays.toString(leftOver2B.toArray()));
         }
+        
+        //If user inputs anything other than 1 or 2.
         else {
             System.out.println("Please only enter 1 or 2.");
         }
